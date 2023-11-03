0c6395a63e0e830f1898de9b3840476fabaf5a0a
Alexander <125283933+Chixablox@users.noreply.github.com>
2023-11-03 10:39:37 +0000

    Fix Angle

--- SpaceBattle.Lib/Angle.cs
+++ SpaceBattle.Lib/Angle.cs
@@ -3,16 +3,15 @@ namespace SpaceBattle.Lib;
 public class Angle
 {
     public int dir { get; set; }
-    public int num { get; set; }
+    public int num { get;} = 8;
 
-    public Angle(int d, int n)
+    public Angle(int d)
     {
         this.dir = d;
-        this.num = n;
     }
 
     public static Angle operator +(Angle a1, Angle a2)
     {
-        return new Angle((a1.dir + a2.dir)%a1.num, a1.num);
+        return new Angle((a1.dir + a2.dir)%a1.num);
     }
 }
